170171503948eb6e5644f358e86ee72b49c5bce5
Daniil Polovinkin <Niades@users.noreply.github.com>
2022-07-19 12:10:07 +0400

    chore(test): add "load video" test case

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,21 @@
 /**
  * @jest-environment jsdom
  */
-
 import { YouTubePlayer } from "../src/index"
+
+const TEST_VIDEO_ID = "dQw4w9WgXcQ";
 
 test('attaches to a DOM node', () => {
   const div = document.createElement("div")
   div.setAttribute("id", "player");
   const player = new YouTubePlayer(div);
   expect(player.getState()).toBe("unstarted")
+});
+
+test('loads video', () => {
+  const div = document.createElement("div")
+  div.setAttribute("id", "player");
+  const player = new YouTubePlayer(div);
+  player.load(TEST_VIDEO_ID);
+  expect(player.videoId).toBe(TEST_VIDEO_ID)
 });
